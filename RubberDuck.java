0a9018b1e7ca8173436f6942fc973caf26c0955e
Lakshya <89395485+joshilakshya@users.noreply.github.com>
2025-03-15 14:39:15 +0530

    Create RubberDuck.java

---
+++ RubberDuck.java
@@ -0,0 +1,11 @@
+// RubberDuck.java
+class RubberDuck extends Duck {
+    public RubberDuck() {
+        super(new NotFly(), new Float());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Rubber Duck.");
+    }
+}
