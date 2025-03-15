9aa0c29711f6ee6c56ab636b9f6e03980a8798c0
Lakshya <89395485+joshilakshya@users.noreply.github.com>
2025-03-15 14:36:50 +0530

    Create DecoyDuck.java

---
+++ DecoyDuck.java
@@ -0,0 +1,11 @@
+// DecoyDuck.java
+class DecoyDuck extends Duck {
+    public DecoyDuck() {
+        super(new NotFly(), new Drown());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Decoy Duck.");
+    }
+}
