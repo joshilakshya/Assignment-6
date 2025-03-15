896067052e350cf78b4b60cbfa378a42b58616b0
Lakshya <89395485+joshilakshya@users.noreply.github.com>
2025-03-15 14:38:49 +0530

    Create MallardDuck.java

---
+++ MallardDuck.java
@@ -0,0 +1,11 @@
+// MallardDuck.java
+class MallardDuck extends Duck {
+    public MallardDuck() {
+        super(new Fly(), new Swim());
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I am a Mallard Duck.");
+    }
+}
